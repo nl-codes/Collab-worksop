d93f6662c16956df8c34a19d1e7f76e76d941a26
narayanLohani <narayanlohani07@gmail.com>
2025-03-21 08:28:03 +0545

    updated divide.py

--- divide.py
+++ divide.py
@@ -1,11 +1,11 @@
-def divide():
+def division():
     num1 = float(input("Enter the first num: "))
     num2 = float(input("Enter the second num: "))
-    divide = num1/num2
     if num2==0:
         result = "Undefined"
     else:
+        divide = num1/num2
         result = divide
-    print(f"The result of {num1} / {num2} is: {result}") 
-    
-divide()
+    print(f"The result of {num1} / {num2} is: {result}")
+
+division()
